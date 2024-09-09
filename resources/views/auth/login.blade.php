27f48a78abad469cc7e4ff5502b4a037f48d5458
barman <barmansystemsp@gmail.com>
2024-09-09 10:17:49 +0330

    changeTitleOfLogin

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
 <html lang="fa" dir="rtl" data-topbar-color="brand">
     <head>
         <meta charset="utf-8" />
-        <title>پرسو تجارت | ورود</title>
+        <title>آداک همراه | ورود</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <!-- App favicon -->
